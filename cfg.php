f872a9c873b0d01d5f935ec87c56336815c8a4a8
Sage Russell <webmaster@sageth.com>
2017-07-24 19:51:16 -0400

    Force https

--- cfg.php
+++ cfg.php
@@ -44,45 +44,45 @@ define('TOP_SCORE_COUNT', 10);
 /* ===== LIBRARIES USED THROUGHOUT THE SITE */
 
 /* CDNJS - v3.3.7 - BOOTSTRAP */
-define('CSS_BOOTSTRAP', '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css');
-define('CSS_BOOTSTRAP_THEME', '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css');
-define('JS_BOOTSTRAP', '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js');
+define('CSS_BOOTSTRAP', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css');
+define('CSS_BOOTSTRAP_THEME', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css');
+define('JS_BOOTSTRAP', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js');
 
 /* CDNJS - v2.2.2 - BOOTSTRAP TOGGLE */
-define('CSS_BOOTSTRAP_TOGGLE', '//cdnjs.cloudflare.com/ajax/libs/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css');
-define('JS_BOOTSTRAP_TOGGLE', '//cdnjs.cloudflare.com/ajax/libs/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js');
+define('CSS_BOOTSTRAP_TOGGLE', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css');
+define('JS_BOOTSTRAP_TOGGLE', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js');
 
 /* CDNJS - v4.7.0 - FONT AWESOME */
-define('CSS_FONTAWESOME', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+define('CSS_FONTAWESOME', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
 
 /* GOOGLE RECAPTCHA */
-define('JS_GOOGLE_RECAPTCHA', '//www.google.com/recaptcha/api.js');
+define('JS_GOOGLE_RECAPTCHA', 'https://www.google.com/recaptcha/api.js');
 
 /* CDNJS - v3.2.1 - JQUERY */
-define('JS_JQUERY', '//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js');
+define('JS_JQUERY', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js');
 
 /* CDNJS - v1.12.1 - JQUERY UI */
-define('JS_JQUERY_UI', '//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js');
+define('JS_JQUERY_UI', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js');
 
 /* CDNJS - v2.7.0 - JQUERY METISMENU */
-define('CSS_METISMENU', '//cdnjs.cloudflare.com/ajax/libs/metisMenu/2.7.0/metisMenu.min.css');
-define('JS_METISMENU', '//cdnjs.cloudflare.com/ajax/libs/metisMenu/2.7.0/metisMenu.min.js');
+define('CSS_METISMENU', 'https://cdnjs.cloudflare.com/ajax/libs/metisMenu/2.7.0/metisMenu.min.css');
+define('JS_METISMENU', 'https://cdnjs.cloudflare.com/ajax/libs/metisMenu/2.7.0/metisMenu.min.js');
 
 /* CDNJS - v3.3.7+1 SB ADMIN 2 THEME */
-define('CSS_SB_ADMIN_2', '//cdnjs.cloudflare.com/ajax/libs/startbootstrap-sb-admin-2/3.3.7+1/css/sb-admin-2.min.css');
-define('JS_SB_ADMIN_2', '//cdnjs.cloudflare.com/ajax/libs/startbootstrap-sb-admin-2/3.3.7+1/js/sb-admin-2.min.js');
+define('CSS_SB_ADMIN_2', 'https://cdnjs.cloudflare.com/ajax/libs/startbootstrap-sb-admin-2/3.3.7+1/css/sb-admin-2.min.css');
+define('JS_SB_ADMIN_2', 'https://cdnjs.cloudflare.com/ajax/libs/startbootstrap-sb-admin-2/3.3.7+1/js/sb-admin-2.min.js');
 
 /* CDNJS - v2.2.0 - SWFOBJECT */
-define('JS_SWFOBJECT', '//cdnjs.cloudflare.com/ajax/libs/swfobject/2.2/swfobject.min.js');
+define('JS_SWFOBJECT', 'https://cdnjs.cloudflare.com/ajax/libs/swfobject/2.2/swfobject.min.js');
 
 /* CUSTOM - TABLESORT */
 define('JS_TABLESORT', SITE_URL . 'plugins/site/themes/admin/assets/js/tablesort.min.js');
 
 /* STANDARD URLS FOR EXTERNAL SITES */
-define('URL_FACEBOOK', '//www.facebook.com/');
-define('URL_GITHUB', '//github.com/');
-define('URL_GITHUB_PHPARCADE', '//github.com/Sageth/phpArcade/');
-define('URL_TWITTER', '//www.twitter.com/');
+define('URL_FACEBOOK', 'https://www.facebook.com/');
+define('URL_GITHUB', 'https://github.com/');
+define('URL_GITHUB_PHPARCADE', 'https://github.com/Sageth/phpArcade/');
+define('URL_TWITTER', 'https://www.twitter.com/');
 /* ====== END CONSTANTS ===== */
 
 /* Load vendor/composer downloaded functions */
