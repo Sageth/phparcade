c1e776fb87138bedc67d1b7b4584732541a6f25e
Ben Chrisman <me@bennyjake.com>
2017-10-08 18:31:47 -0500

    add router to index.php, handle game/id/game_name redirection if game_name doesnt match actual name of game

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
 require_once __DIR__ . '/cfg.php';
 
+$router = new Phroute\Phroute\RouteCollector();
+$router->any(['/game/{id:i}/{passedName:c}', 'game'], function ($id, $passedName) {
+    $game = Games::getGame($id);
+    $actualName = $game['name'];
+    $actualNameWithHtml = $actualName . '.html';
+    if($actualNameWithHtml != $passedName){
+        header('Location: /game/'.$id.'/'.$actualNameWithHtml);
+        return false;
+    }
+    else{
+        $_GET['params'] = 'game/'.$id.'/'.$actualName;
+
+    }
+});
+
+$response = (new Phroute\Phroute\Dispatcher($router->getData()))->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
+
 /* Enable debug logging in non-prod */
 $inicfg = Core::getInstance()->getINIConfig();
 if ($inicfg['environment']['state'] === "dev") {
@@ -16,3 +33,4 @@ Core::doEvent('theme_display');
 /** @noinspection PhpUndefinedVariableInspection */
 /** @noinspection PhpIncludeInspection */
 include SITE_THEME_PATH;
+
