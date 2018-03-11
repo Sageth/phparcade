41ec883e080a257404f86830f56ec141a353f00b
Sage Russell <webmaster@sageth.com>
2018-03-10 22:36:53 -0500

    Update Language tests

--- tests/LanguageTest.php
+++ tests/LanguageTest.php
@@ -19,7 +19,6 @@ final class LanguageTest extends TestCase
     public function testLanguageLoader(): void
     {
         $locale = 'en_US';
-        putenv("LC_ALL=$locale");
         setlocale(LC_ALL, $locale);
         bindtextdomain($locale, $_SERVER['DOCUMENT_ROOT'] . '/includes/locale');
         textdomain($locale);
