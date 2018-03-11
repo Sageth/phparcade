8944d98098a27472af2f896b01b4a59d271ceda7
Sage Russell <webmaster@sageth.com>
2018-03-10 20:05:39 -0500

    Add first unit tests

---
+++ tests/UsersTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class UsersTest extends TestCase
+{
+    public function setUp()
+    {
+        $_SERVER['DOCUMENT_ROOT'] = '/www/phparcade';
+        include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/classes/Core.php';
+        include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/classes/Users.php';
+    }
+
+    public function tearDown()
+    {
+        unset($_SERVER['DOCUMENT_ROOT']);
+    }
+
+    public function testUserPasswordHash(): void
+    {
+        $hash = password_hash('password', PASSWORD_DEFAULT);
+        $this->assertEquals($hash, crypt('password', $hash));
+    }
+    public function testUserPasswordGenerate(): void
+    {
+        $hexPassword = 'password';
+        $this->assertEquals('70617373776f7264', bin2hex($hexPassword));
+    }
+}
