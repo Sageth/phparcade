cf6198907173f474582395e04309f32daf8c5488
Sage Russell <webmaster@sageth.com>
2018-05-13 20:41:25 -0400

    Update tag manager to variable

--- plugins/site/themes/bootstrap4/index.php
+++ plugins/site/themes/bootstrap4/index.php
@@ -24,11 +24,11 @@ require_once __DIR__ . '/themeconfig.php';
                         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WBR3T7');
+            })(window,document,'script','dataLayer','<?php echo $dbconfig['ga_id'];?>');
             </script>
             <!-- End Google Tag Manager -->
         <?php } ?>
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <script>
             (adsbygoogle = window.adsbygoogle || []).push({
                 google_ad_client: "ca-pub-8727545858461215",
