b3ab81f3ca148d21e7feababa1a39b3ca67a4669
Sage Russell <webmaster@sageth.com>
2018-10-02 12:57:24 -0400

    Migrate scores from theme function to class

--- includes/classes/Scores.php
+++ includes/classes/Scores.php
@@ -197,6 +197,36 @@ class Scores
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         return curl_exec($curl);
     }
+    public static function registerScoreSystem(){
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+        $_GET['act'] = $_GET['act'] ?? '';
+        if ($_GET['act'] == 'Arcade' && $_GET['do'] == 'newscore') { //v2 games
+            /* 'gname' comes from the submission in Flash and is equal to `game`.`nameid` */
+            $game = Games::getGameByNameID($_POST['gname']);
+
+            /* Get the game flags to determine scoring type */
+            $sort = Scores::getScoreType('lowhighscore', $game['flags']) ? 'ASC' : 'DESC';
+
+            /* Get the game link */
+            $link = Core::getLinkGame($game['id']);
+
+            if (!$_SESSION) {
+                Core::loadRedirect($link);
+            } else {
+                if (isset($_POST['gname'], $_POST['gscore'])) {
+                    if ($_POST['gscore'] <= 0) {
+                        Core::loadRedirect($link);
+                    }
+                    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'];
+                    Scores::submitGameScore($game['id'], $_POST['gscore'], $_SESSION['user']['id'], $ip, $link, $sort);
+                } else {
+                    Core::loadRedirect($link);
+                }
+            }
+        }
+    }
     public static function submitGameScore($gameid = '', $score = 0, $playerid = '', $ip = '1.1.1.1', $link, $sort = 'DESC')
     {
         $time = Core::getCurrentDate();
