196c7dc5995b84e1e6470de630e2d0e1b55d43ff
Sage Russell <webmaster@sageth.com>
2018-10-02 13:44:38 -0400

    Implement user profile routing

--- index.php
+++ index.php
@@ -6,14 +6,29 @@ $router = new Phroute\Phroute\RouteCollector();
 
 // Game Routing
 // catch http://phparcade.dev/game/2741/Sample.html
-$router->any(['/game/{id:i}/{passedName:.*}', 'game'], function ($id, $passedName) use (&$foundMatch) {
+$router->get(['/game/{id:i}/{gameName:.*}', 'game'], function ($id, $gameName) use (&$foundMatch) {
     $game = PHPArcade\Games::getGame($id);
-    $actualNameWithHtml = PHPArcade\Core::getCleanURL($game['name']) . '.html';
-    if ($actualNameWithHtml != urldecode($passedName)) {
-        header('Location: /game/'.$id.'/'.urlencode($actualNameWithHtml));
+    $cleanGameName = PHPArcade\Core::getCleanURL($game['name']) . '.html';
+    if ($cleanGameName != urldecode($gameName)) {
+        header('Location: /game/'.$id.'/'.urlencode($cleanGameName));
         return false;
     } else {
         $_GET['params'] = 'game/'.$id.'/'. PHPArcade\Core::getCleanURL($game['name']);
+    }
+});
+
+// User Profile Routing
+// catch http://phparcade.dev/profile/view/1/Username.html
+$router->get(['/profile/view/{id:i}/{userProfile:.*}', 'user'], function ($id, $userProfile) use (&$foundMatch) {
+    $user = PHPArcade\Users::getUserbyID($id);
+    $cleanUserName = PHPArcade\Core::getCleanURL($user['username']) . '.html';
+    if ($cleanUserName != urldecode($userProfile))
+    {
+        header('Location: /profile/view/' . $id . '/' . urlencode($cleanUserName));
+        return false;
+    } else
+    {
+        $_GET['params'] = 'profile/view/' . $id . '/' . PHPArcade\Core::getCleanURL($user['username']);
     }
 });
 
